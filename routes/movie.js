41b96f96b31ede2fe23f7ce603d7f6028f02e97d
TheInfinity007 <ramchandra71999@gmail.com>
2020-05-24 01:06:04 +0530

    add the Trending Movie route

--- routes/movie.js
+++ routes/movie.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const request = require('request');
 const ExternalIds = require("../models/externalId");
 
+var start;
 
-var externalIdsData = {};
 
 getImdbId = function(res, mId){
 	let url = `https://api.themoviedb.org/3/movie/${mId}/external_ids?api_key=1b58a6bfefb9d8ebd9a671fc53e4e9c9`;
@@ -16,8 +16,7 @@ getImdbId = function(res, mId){
 			console.log(imdbId);
 			ExternalIds.create({tmdbId: mId, imdbId: imdbId}, (err, obj)=>{
 				console.log("New = " + obj);
-			})
-			// externalIdsData[mId] = imdbId;
+			});
 			grabMovieData(res, imdbId);
 		}else{
 			res.send("ERROR OCCURED");
@@ -26,22 +25,28 @@ getImdbId = function(res, mId){
 }
 
 grabMovieData = function(res, imdbId){
+	if(imdbId == null){
+		res.send("Data Not Found Exit 1")
+	}
 	url = `http://www.omdbapi.com/?i=${imdbId}&plot=full&apikey=thewdb`;
+	console.log(url);
 	request(url, (error, response, body)=>{
 		if(!error && response.statusCode == 200){
 			let data = JSON.parse(body);
-			console.log(new Date().getTime()-start);
-			console.log(data);
-			res.render("show", {data: data});
+			if(data.Response == 'True'){
+				console.log("Data = ", data);
+				console.log(new Date().getTime()-start);
+				res.render("show", {data: data});
+			}else{
+				res.send("Data Not Found Exit 2");
+			}
 		}else{
+			if(error)console.log("Error = ", error);
+			if(response){console.log("Status Code = ", response.statusCode);}
 			res.send("ERROR OCCURED IN FETCHING THE DATA");
 		}
 	});
 }
-
-
-
-
 
 getMovies = function(url, pageTitle, pageUrl, pageNo, res){
 	request(url, (error, response, body)=>{
@@ -58,6 +63,7 @@ getMovies = function(url, pageTitle, pageUrl, pageNo, res){
 				temp.push(result["poster_path"]);
 				movies.push(temp);
 			});
+			console.log(new Date().getTime() - start);
 			res.render("movie", {
 				data: movies, 
 				pageTitle: pageTitle,
@@ -73,7 +79,7 @@ getMovies = function(url, pageTitle, pageUrl, pageNo, res){
 
 // /movie route
 router.get('/', (req, res)=>{
-	let popularMovies = [];
+	start = new Date().getTime();
 	let pageNo = 1;
 	if(req.query.page){
 		pageNo = parseInt(req.query.page);
@@ -85,6 +91,7 @@ router.get('/', (req, res)=>{
 });
 
 router.get('/in-theatre', (req, res)=>{
+	start = new Date().getTime();
 	let pageNo = 1;
 	if(req.query.page){
 		pageNo = parseInt(req.query.page);
@@ -96,6 +103,7 @@ router.get('/in-theatre', (req, res)=>{
 });
 
 router.get('/evergreen', (req, res)=>{
+	start = new Date().getTime();
 	let pageNo = 1;
 	if(req.query.page){
 		pageNo = parseInt(req.query.page);
@@ -107,6 +115,7 @@ router.get('/evergreen', (req, res)=>{
 });
 
 router.get('/upcoming', (req, res)=>{
+	start = new Date().getTime();
 	let pageNo = 1;
 	if(req.query.page){
 		pageNo = parseInt(req.query.page);
@@ -116,6 +125,18 @@ router.get('/upcoming', (req, res)=>{
 	let pageUrl = "upcoming";
 	getMovies(url, pageTitle, pageUrl, pageNo, res);
 });
+
+router.get('/trending', (req, res)=>{
+	start = new Date().getTime();
+	let pageNo = 1;
+	if(req.query.page){
+		pageNo = parseInt(req.query.page);
+	}
+	let url = `https://api.themoviedb.org/3/trending/movie/day?page=${pageNo}&api_key=1b58a6bfefb9d8ebd9a671fc53e4e9c9`;
+	let pageTitle = "Trending";
+	let pageUrl = "trending";
+	getMovies(url, pageTitle, pageUrl, pageNo, res);
+})
 
 router.get('/:movie_id', async (req, res)=>{
 	start = new Date().getTime();

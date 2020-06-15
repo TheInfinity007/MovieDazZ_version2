04f3c66abc2a15016a60a1d31b2662e610e12fcc
TheInfinity007 <ramchandra71999@gmail.com>
2020-06-16 01:22:42 +0530

    Completed the project finally

--- public/js/script.js
+++ public/js/script.js
@@ -13,4 +13,19 @@ searchForm.addEventListener("submit", function(event){
 $(document).ready(function() {
 	var pathname = window.location.pathname;
 	$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+
+	// show hide button on scroll
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 200){
+			$('.scrollToTop').fadeIn();
+		}else{
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	// smooth scrolling to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop: 0	}, 500);
+	})
+
 });
